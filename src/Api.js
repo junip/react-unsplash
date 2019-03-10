2e857ce3ab1b3a19454d1771d1c30bcc80b31a83
junipdewan <junipd2@gmail.com>
2019-03-10 17:32:49 +0530

    Typo fixes

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,7 @@
 /**
  * -------------------------------------------------------------
  * Importing Unsplash object and using for fetching image data using
- * APP_KEY and APP_SECRET 
+ * APP_KEY and APP_SECRET
  * SignUp Yourself in `https://unsplash.com/` GO TO Threedots -> API/Devlopers
  * Follow the instruction and API USE restriction to use the API KEY
  * -------------------------------------------------------------
@@ -10,16 +10,16 @@
 /**
  *  Github Link for the unsplash-js
  *  `https://github.com/unsplash/unsplash-js`
- */ 
-import Unsplash from 'unsplash-js';
+ */
 
-APP_ACCESS_KEY = process.env.APP_ACCESS_KEY;
-APP_SECRET = process.env.APP_SECRET;
+import Unsplash from "unsplash-js";
 
+const APP_ACCESS_KEY = process.env.API_KEY;
+const API_SECRET = process.env.API_SECRET;
 
 const unsplash = new Unsplash({
-    applicationId: APP_ACCESS_KEY,
-    secret: APP_SECRET
+  applicationId: APP_ACCESS_KEY,
+  secret: API_SECRET
 });
 
 export default unsplash;
