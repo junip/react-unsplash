68153120dd2c4ad0ba7abe1e3812fdb303bad21a
junipdewan <junipd2@gmail.com>
2019-03-24 00:58:06 +0530

    Adde Button Style

---
+++ src/Styles/ButtonStyle.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "aphrodite/no-important";
+
+const buttonStyle = StyleSheet.create({
+  button: {
+    "font-size": "14px",
+    "line-height": "30px",
+    cursor: "pointer !important",
+    "border-radius": "4px",
+    color: "#777",
+    "background-color": "#ddd!important",
+    border: "1px solid transparent",
+    height: "32px",
+    padding: "0 11px",
+    "font-size": "14px",
+    "line-height": "30px"
+  }
+});
+
+export default buttonStyle;
