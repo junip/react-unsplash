0aa8681e59147cf5c51b37bbf9fecf0b52cbbe6c
junipdewan <junipd2@gmail.com>
2019-03-25 00:47:52 +0530

    style sheet refactoring and changes

--- src/Photo.js
+++ src/Photo.js
@@ -4,34 +4,38 @@ import buttonStyle from "./Styles/ButtonStyle";
 import download from "./images/download_icon.png";
 
 const phtoStyle = StyleSheet.create({
+  unsplashPhotoDiv: {
+    width: "100%",
+    overflow: "hidden",
+    position: "relative",
+    ":hover": {
+      cursor: "zoom-in"
+    }
+  },
+  likesDiv: {
+    display: "flex",
+    "justify-content": "flex-end",
+    position: "absolute",
+    right: "20px",
+    top: "20px"
+  },
+  heartIcon: {
+    color: "#f54b48"
+  },
   imageStyle: {
     width: "100%",
     display: "block",
     borderRadius: "2px"
   },
-  imageDiv: {
-    ":hover": {
-      cursor: "zoom-in"
-    }
-  },
-  inlineDiv: {
+  footerDiv: {
     display: "flex",
     position: "absolute",
     bottom: "20px",
     left: "20px"
   },
-  heartIcon: {
-    color: "#f54b48"
-  },
   buttonMargin: {
     "margin-left": "8px"
   },
-  toRight: {
-    right: "45px"
-  },
-
-  lowerDiv: {},
-
   fullNameCss: {
     color: "white",
     position: "relative",
@@ -40,18 +44,7 @@ const phtoStyle = StyleSheet.create({
   userIcon: {
     "border-radius": "18px"
   },
-  upperDiv: {
-    display: "flex",
-    "justify-content": "flex-end",
-    position: "absolute",
-    right: "20px",
-    top: "20px"
-  },
-  placeRight: {
-    float: "right"
-  },
-
-  downloadDiv: {
+  downloadPhoto: {
     display: "flex",
     "justify-content": "flex-end",
     position: "absolute",
@@ -64,18 +57,13 @@ const phtoStyle = StyleSheet.create({
     "border-radius": "4px",
     "padding-top": "7px",
     "justify-content": "center"
-  },
-  photoRenderDiv: {
-    width: "100%",
-    overflow: "hidden",
-    position: "relative"
   }
 });
 const photo = props => {
   const { photoUrl, likes, profilePhoto, fullName, downloadUrl } = props;
   return (
-    <div className={css(phtoStyle.photoRenderDiv)}>
-      <div className={css(phtoStyle.upperDiv)}>
+    <div className={css(phtoStyle.unsplashPhotoDiv)}>
+      <div className={css(phtoStyle.likesDiv)}>
         <div className={css(buttonStyle.button)}>
           <span className={css(phtoStyle.heartIcon)}>
             <i className="fas fa-heart" />
@@ -87,13 +75,13 @@ const photo = props => {
         </div>
       </div>
       <img src={photoUrl} className={css(phtoStyle.imageStyle)} />
-      <div className={css(phtoStyle.inlineDiv)}>
+      <div className={css(phtoStyle.footerDiv)}>
         <img src={profilePhoto} className={css(phtoStyle.userIcon)} />
         <div className={css(phtoStyle.buttonMargin)}>
           <span className={css(phtoStyle.fullNameCss)}>{fullName}</span>
         </div>
       </div>
-      <div className={css(phtoStyle.downloadDiv)}>
+      <div className={css(phtoStyle.downloadPhoto)}>
         <a
           className={css(buttonStyle.button)}
           href={`${downloadUrl}?force=true`}
